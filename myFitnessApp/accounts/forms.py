da329aa96fe31b889c9e99605d450128c56ef0e9
Ivaylo1992 <ivailop123@gmail.com>
2025-02-19 06:49:07 +0200

    fix: customized the user admin

---
+++ myFitnessApp/accounts/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class FitnessAppUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = UserModel
+
+
+class FitnessAppUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = UserModel
+        fields = ('email', )
