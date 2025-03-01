d3a8ac78b729d2db468b754a8cca25b6e817253a
Ivaylo1992 <ivailop123@gmail.com>
2025-03-01 06:38:22 +0200

    feat: added exercise model

--- myFitnessApp/workouts/models.py
+++ myFitnessApp/workouts/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    class MuscleGroupChoices(models.TextChoices):
+        ABDOMINALS = 'Abdominals'
+        ABDUCTORS = 'Abductors'
+        BICEPS = 'Biceps'
+        CALVES = 'Calves'
+        CHEST = 'Chest'
+        FOREARMS = 'Forearms'
+        GLUTES = 'Glutes'
+        HAMSTRINGS = 'Hamstrings'
+        LATS = 'Lats'
+        LOWER_BACK = 'Lower back'
+        MIDDLE_BACK = 'Middle back'
+        NECK = 'Neck'
+        QUADRICEPS = 'Quadriceps'
+        TRAPS = 'Traps'
+        TRICEPS = 'Triceps'
+        OTHER = 'Other'
+
+    NAME_MAX_LENGTH = 50
+    MUSCLE_GROUP_MAX_LENGTH = max([len(x) for _, x in MuscleGroupChoices.choices])
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+    )
+
+    MuscleGroup = models.CharField(
+        max_length=MUSCLE_GROUP_MAX_LENGTH,
+        choices=MuscleGroupChoices.choices,
+        default=MuscleGroupChoices.OTHER
+    )
+
+    exercise_picture = models.URLField(
+        null=True,
+        blank=True,
+    )
+
+    exercise_video = models.URLField(
+        null=True,
+        blank=True
+    )
+
+    instructions = models.TextField(
+        null=True,
+        blank=True
+    )
