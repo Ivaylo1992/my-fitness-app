a51add6106a67a5ae3cfb18f9cabaed81dc65a86
Ivaylo1992 <ivailop123@gmail.com>
2025-03-07 06:29:02 +0200

    added BodyMeasurements and BodyCompositionLog to the admin

--- myFitnessApp/bodycompositions/admin.py
+++ myFitnessApp/bodycompositions/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 
+from myFitnessApp.bodycompositions.models import BodyCompositionLog, BodyMeasurements
+
+@admin.register(BodyCompositionLog)
+class BodyCompositionLogAdmin(admin.ModelAdmin):
+    ...
 
 
+@admin.register(BodyMeasurements)
+class BodyMeasurementsAdmin(admin.ModelAdmin):
+    ...
