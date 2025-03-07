9b7b13516846228bc9ed453947f97c1893ac19df
Ivaylo1992 <ivailop123@gmail.com>
2025-03-07 06:25:51 +0200

    feat: created BodyCompositionLog and BodyMeasurements models

--- myFitnessApp/bodycompositions/models.py
+++ myFitnessApp/bodycompositions/models.py
@@ -1,3 +1,105 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+from myFitnessApp.utils.mixins import HasUserMixin, TimeStampedMixin
+
+
+class BodyCompositionLog(TimeStampedMixin, HasUserMixin):
+    weight = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(499.9)
+        )
+    )
+
+    body_fat = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(99.9)
+        ),
+        null=True,
+        blank=True,
+    )
+
+
+class BodyMeasurements(TimeStampedMixin, HasUserMixin):
+    neck = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(49.9)
+        ),
+        null=True,
+        blank=True,
+    )
+
+    chest = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(149.9)
+        ),
+        null=True,
+        blank=True,
+    )
+
+    biceps = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(99.9)
+        ),
+        null=True,
+        blank=True,
+    )
+
+    waist = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(299.9)
+        ),
+        null=True,
+        blank=True,
+    )
+
+    hips = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(299.9)
+        ),
+        null=True,
+        blank=True,
+    )
+
+    thigh =models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(99.9)
+        ),
+        null=True,
+        blank=True,
+    )
+
+    calf =models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(99.9)
+        ),
+        null=True,
+        blank=True,
+    )
