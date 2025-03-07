0eee03b80fdada2e39f3ec8d84ae17d6d17b4945
Ivaylo1992 <ivailop123@gmail.com>
2025-03-07 05:47:36 +0200

    fix: changed swagger UI to be homepage

--- myFitnessApp/urls.py
+++ myFitnessApp/urls.py
@@ -14,11 +14,7 @@ urlpatterns = [
     path("workouts/", include("myFitnessApp.workouts.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path(
         "api/schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
