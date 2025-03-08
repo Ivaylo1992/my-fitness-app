509c288aaabf3ac7397e9895c2adca1246969f47
Ivaylo1992 <ivailop123@gmail.com>
2025-03-08 06:42:09 +0200

    added swagger documentation to BodyCompositionLogViewSet and BodyMeasurementsLogViewSet

--- myFitnessApp/bodycompositions/views.py
+++ myFitnessApp/bodycompositions/views.py
@@ -1,8 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema
 
 from myFitnessApp.bodycompositions.models import BodyCompositionLog, BodyMeasurements
 from myFitnessApp.bodycompositions.serializers import BodyCompositionLogSerializer, BodyMeasurementsSerializer
 
+@extend_schema(
+    tags=["weight ins"],
+    summary="Weight in endpoint",
+    description="All CRUD operations for the BodyCompositionLog model.",
+)
 class BodyCompositionLogViewSet(ModelViewSet):
     serializer_class = BodyCompositionLogSerializer
     queryset = BodyCompositionLog.objects.all()
@@ -11,6 +17,11 @@ class BodyCompositionLogViewSet(ModelViewSet):
         return serializer.save(user=self.request.user)
 
 
+@extend_schema(
+    tags=["body measurements"],
+    summary="Body Measurements endpoint",
+    description="All CRUD operations for the BodyMeasurements model.",
+)
 class BodyMeasurementsLogViewSet(ModelViewSet):
     serializer_class = BodyMeasurementsSerializer
     queryset = BodyMeasurements.objects.all()
